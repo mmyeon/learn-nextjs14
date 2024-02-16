8cb3120326d10e0567c0488391e9c5c1c3e2c337
mmyeon <mmyeon@mmyeonui-MacBookPro.local>
2024-02-16 22:56:02 +0900

    [feat] Add page error handling

--- app/components/moview-videos.tsx
+++ app/components/moview-videos.tsx
@@ -2,7 +2,8 @@ import { API_URL } from "../(home)/page";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos ${Date.now()}`);
-  //   await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error(`Somethign broken...`);
   const reposne = await fetch(`${API_URL}/${id}/videos`);
   const json = await reposne.json();
 
