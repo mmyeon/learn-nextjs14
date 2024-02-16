5a83431e6d68b82b3221622addb655098f12a23e
mmyeon <mmyeon@mmyeonui-MacBookPro.local>
2024-02-16 22:13:04 +0900

    [feat] Fetch data in parallel

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,35 @@
-export default function MovieDetail({
+import { Metadata } from "next";
+import { API_URL } from "../../../(home)/page";
+
+export const metadata: Metadata = {
+  title: "Movie Detail",
+};
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const reponse = await fetch(`${API_URL}/${id}`);
+  const json = await reponse.json();
+
+  return json;
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  const reposne = await fetch(`${API_URL}/${id}/videos`);
+  const json = await reposne.json();
+
+  return json;
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  console.log(`Start fetching`);
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log(`End fetching`);
+  return <h1>{movie.title}</h1>;
 }
