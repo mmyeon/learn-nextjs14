55e523b92e9138c82211dc183c12f5edf156cc5c
mmyeon <mmyeon@mmyeonui-MacBookPro.local>
2024-02-18 17:48:26 +0900

    [styles] Apply movie info and video trailer style

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
   console.log(`Fetching movies ${Date.now()}`);
@@ -10,5 +11,17 @@ async function getMovie(id: string) {
 }
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img className={styles.poster} src={movie.poster_path} />
+      <div>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3> ⭐ {movie.vote_average.toFixed(2)}</h3>
+        <p className={styles.info}>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
